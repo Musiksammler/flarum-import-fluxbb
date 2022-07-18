e226ef87a03246d2f10d59ad18d55c659efb9393
Musiksammler <carsten.henkelmann@musik-sammler.de>
2022-07-18 11:03:45 +0200

    Clear one more table during import first.

--- src/Importer/InitialCleanup.php
+++ src/Importer/InitialCleanup.php
@@ -35,6 +35,7 @@ class InitialCleanup
 
         $this->database->statement('TRUNCATE TABLE '.$this->database->getTablePrefix().'groups');
         $this->database->statement('TRUNCATE TABLE '.$this->database->getTablePrefix().'group_user');
+        $this->database->statement('TRUNCATE TABLE '.$this->database->getTablePrefix().'group_permission');
 //        $this->database->statement('TRUNCATE TABLE '.$this->database->getTablePrefix().'discussions');
         $this->database->statement('TRUNCATE TABLE '.$this->database->getTablePrefix().'discussion_tag');
         $this->database->statement('TRUNCATE TABLE '.$this->database->getTablePrefix().'discussion_user');
